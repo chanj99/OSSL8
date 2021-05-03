de1718d9976e0cf3486340e6b0be8b783b99befc
LeeDonggyu <asasd456123@naver.com>
2021-05-03 21:57:26 +0900

    change computerCourse.h

--- computerCourse.h
+++ computerCourse.h
@@ -13,8 +13,11 @@ int deleteCourse(computerCourse *c); //수업 삭제
 
 void savaData(computerCourse *c, int count); //파일 저장 기능
 int loadData(computerCourse *c); //파일 불러오기 기능
-void searchCourse(computerCourse *c); //과목이름과 학점으로 검색하는 기능
-void showchoose(computerCourse c); //내가 담은 과목만 조회할 수 있는 기능 추가
+void searchCourseByName(computerCourse *c,int count); //과목이름으로 검색하는 기능
+void searchCourseByCredit(computerCourse *c,int count); //학점으로 검색하는 기능
+void addInCart(computerCourse *cart, computerCourse c); //선택한 과목을 장바구니에 담는 기능
+void showchoose(computerCourse *cart, int count); //내가 담은 과목만 조회할 수 있는 기능 추가
+
 
 void listCourse(computerCourse *c, int count); //수업목록 전체 읽기
 int selectMenu(); //처음 메뉴 선택
