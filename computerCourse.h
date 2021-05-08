431c52589fc91bededdf0a2d34c99f6c36747d5b
chanjulee <jenny991119@naver.com>
2021-05-08 16:03:13 +0900

    헤더파일 수정

--- computerCourse.h
+++ computerCourse.h
@@ -11,12 +11,12 @@ void readCourse(computerCourse c); //수업 목록 읽기
 void updateCourse(computerCourse *c); //수업 목록 업데이트
 int deleteCourse(computerCourse *c); //수업 삭제
 int addInCart(computerCourse *cart, computerCourse c); //선택한 과목을 장바구니에 담는 기능
+void showchoose(computerCourse *cart, int count); //내가 담은 과목만 조회할 수 있는 기능 추가
 /*
 void savaData(computerCourse *c, int count); //파일 저장 기능
 int loadData(computerCourse *c); //파일 불러오기 기능
 void searchCourseByName(computerCourse *c,int count); //과목이름으로 검색하는 기능
 void searchCourseByCredit(computerCourse *c,int count); //학점으로 검색하는 기능
-void showchoose(computerCourse *cart, int count); //내가 담은 과목만 조회할 수 있는 기능 추가
 */
 int selectDataNum(computerCourse *c, int count);
 void listCourse(computerCourse *c, int count); //수업목록 전체 읽기
