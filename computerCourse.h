e054df7b2d88642fb8d1ca2c16ce3e8d8ec81fce
LeeDonggyu <asasd456123@naver.com>
2021-05-17 16:34:38 +0900

    fix comment

--- computerCourse.h
+++ computerCourse.h
@@ -1,9 +1,9 @@
-typedef struct //�İ���ȭ ����
+typedef struct //컴공심화 과정
 {
-    char name[100]; //�����̸�
-    int credit; //����
-    char designCourse; //������� ����(Y or N)
-    char classification[100]; //�̼�����(�����ʼ�, ��������, ���������ʼ�)
+    char name[100]; //과목이름
+    int credit; //학점
+    char designCourse; //설계과목 여부(Y or N)
+    char classification[100]; //이수구분(전공필수, 전공선택, 전공선택필수)
 }computerCourse;
 
 typedef struct{
@@ -13,22 +13,22 @@ typedef struct{
 
 void signUp();
 int login();
-int addCourse(computerCourse *c); //���� ��� �߰��ϱ�
-void readCourse(computerCourse c); //���� ��� �б�
-void updateCourse(computerCourse *c); //���� ��� ������Ʈ
-int deleteCourse(computerCourse *c); //���� ����
-int addInCart(computerCourse *cart, computerCourse c); //������ ������ ��ٱ��Ͽ� ��� ���
-void showchoose(computerCourse *cart, int count); //���� ���� ���� ��ȸ�� �� �ִ� ��� �߰�
-void saveCourse(computerCourse *c, int count); //���� ���� ���
-int loadData(computerCourse *c); //���� �ҷ����� ���
-void searchCourseByCredit(computerCourse *c,int count); //�������� �˻��ϴ� ���
+int addCourse(computerCourse *c); //수업 목록 추가하기
+void readCourse(computerCourse c); //수업 목록 읽기
+void updateCourse(computerCourse *c); //수업 목록 업데이트
+int deleteCourse(computerCourse *c); //수업 삭제
+int addInCart(computerCourse *cart, computerCourse c); //선택한 과목을 장바구니에 담는 기능
+void showchoose(computerCourse *cart, int count); //내가 담은 과목만 조회할 수 있는 기능 추가
+void saveCourse(computerCourse *c, int count); //파일 저장 기능
+int loadData(computerCourse *c); //파일 불러오기 기능
+void searchCourseByCredit(computerCourse *c,int count); //학점으로 검색하는 기능
 
 
-void searchCourseByName(computerCourse *c,int count); //�����̸����� �˻��ϴ� ���
+void searchCourseByName(computerCourse *c,int count); //과목이름으로 검색하는 기능
 
 
 int selectDataNum(computerCourse *c, int count);
-void listCourse(computerCourse *c, int count); //������� ��ü �б�
-int selectMenu(); //ó�� �޴� ����
+void listCourse(computerCourse *c, int count); //수업목록 전체 읽기
+int selectMenu(); //처음 메뉴 선택
 
 
